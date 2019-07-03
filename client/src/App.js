e3f3378ab1f2b4a0ca877507e13e77ef55e10795
salmonshoe <rafaelreynoso273@gmail.com>
2019-07-02 20:41:04 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Park from './pages/Park';
+
+import Navbar from './components/Navbar';
+import Wrapper from './components/Wrapper';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div>
+        <Wrapper>
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/park" component={Park} />
+        </Wrapper>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/Wrapper/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Wrapper() {
+    return (
+        <h1>young bohl u funny</h1>
+    );
+}
+
+export default Wrapper;
