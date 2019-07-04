b4fc0e1c7ec17632c589544baca26b634ec06b04
salmonshoe <rafaelreynoso273@gmail.com>
2019-07-04 18:36:07 -0400

    OAuth components created. Added validation for SignUpForm and /signup works fine!

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,17 +3,6 @@ import React from 'react';
 function Login() {
     return (
         <>
-            <form>
-                <div className="form-group">
-                    <label for="exampleEmail">Email Address</label>
-                    <input type="email" className="form-control" id="exampleEmailInput" placeholder="Enter email"></input>
-                </div>
-                <div className="form-group">
-                    <label for="examplePassword">Password</label>
-                    <input type="password" className="form-control" id="examplePassword" placeholder="Enter password"></input>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
         </>
     );
 }
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,20 +1,10 @@
 import React from 'react';
+import SignUpForm from '../components/SignUpForm';
 
 function SignUp() {
     return (
         <>
-        <h1>Create an Account!</h1>
-            <form>
-                <div className="form-group">
-                    <label for="exampleEmail">Email Address</label>
-                    <input type="email" className="form-control" id="exampleEmailInput" placeholder="Enter email"></input>
-                </div>
-                <div className="form-group">
-                    <label for="examplePassword">Password</label>
-                    <input type="password" className="form-control" id="examplePassword" placeholder="Enter password"></input>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+        <SignUpForm />
         </>
     );
 }
