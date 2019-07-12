028cb30ba94e8f45acded78d38bc14d829f667e5
salmonshoe <rafaelreynoso273@gmail.com>
2019-07-12 16:28:53 -0400

    open the user schema for a first and last name

--- models/User.js
+++ models/User.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: {
+    firstName: {
         type: String,
         required: true
     }, 
+    lastName: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: true
