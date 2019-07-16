3cbd274394497e877d6300aad2b73098180f7873
salmonshoe <rafaelreynoso273@gmail.com>
2019-07-15 20:57:37 -0400

    fix the parks api issue and apply auth to users

--- express-routes/api/index.js
+++ express-routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
-// const parkRoutes = require('./parks');
+const parkRoutes = require('./parks');
 const reviewRoutes = require('./reviews');
 
 // User routes
 router.use('/users', userRoutes);
-// router.use('/parks', parkRoutes);
+router.use('/parks', parkRoutes);
 router.use('/reviews', reviewRoutes);
 
 module.exports = router;
--- express-routes/api/users.js
+++ express-routes/api/users.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const usersController = require('../../controllers/usersController');
-
-// const bcrypt = require('bcryptjs');
-// const passport = require('passport'); // Check if you need to use these dependencies here..
+const db = require('../../models');
+const bcrypt = require('bcryptjs');
+const passport = require('passport'); // Check if you need to use these dependencies here..
 
 // Matches with "/api/users"
 router.route('/')
@@ -14,5 +14,66 @@ router.route('/:id')
     .get(usersController.findById)
     .put(usersController.update)
     .delete(usersController.remove);
+
+// Matches with '/api/users/login' ?
+router.get('/login', (req, res) => res.render('login')); 
+
+// Sign Up handle
+router.post('/register', (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    db.User.findOne({ email: email })
+    .then(user => {
+        if (user) {
+            errors.push({ msg: 'Email is already registered' })
+            Response.render('register', {
+                errors,
+                firstName,
+                lastName, 
+                email,
+                password
+            });
+        } else {
+            const newUser = new User({
+                firstName,
+                lastName,
+                email,
+                password
+            });
+
+            // Hash password. Generate a salt for the hash.Get ready for a callbacl and embedded callback functions
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    // Set password plain text to hashed
+                    newUser.password = hash;
+                    // Save the newUser to MongoDB
+                    newUser.save()
+                    .then(user => {
+                        req.flash('success_msg', 'You are now reigstered and can log in');
+                        res.redirect('/users/login');
+                    })
+                    .catch(err => console.log(err));
+                })
+            })
+        }
+    });
+});
+
+//Login Handle
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
+});
+
+//Logout Handle
+router.get('/logout', (request, response) => {
+    request.logout();
+    request.flash('success_msg', 'You are logged out');
+    response.redirect('/users/login');
+})
+
 
 module.exports = router;
