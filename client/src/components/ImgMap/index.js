f23e0e97ee70377bad40576620f94c204fa97af8
unknown <ben_ngo89@yahoo.com>
2019-07-18 15:46:22 -0400

    add map on park2,css on navbar and park2

---
+++ client/src/components/ImgMap/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+class ImgMap extends React.Component {
+
+    render() {
+        return (
+            <>
+
+ < a href = "https://www.google.com/maps/place/Paine's+Park/@39.9628739,-75.1806211,15z/data=!4m2!3m1!1s0x0:0x527c924ed245fae4?sa=X&ved=2ahUKEwi-4b2b577jAhXGneAKHXpGA1wQ_BIwG3oECA0QCA"
+ target='blank' > < div className = 'bkgndWrapper' >
+                    
+                    
+			</div>
+             </a>
+            </>
+        )
+    }
+} 
+
+
+export default ImgMap;
---
+++ client/src/components/ImgPark/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+
+class ImgPark extends React.Component {
+
+    render() {
+        return (
+            <>
+
+ < div className = 'bkgndWrapper3 bg-primary' >
+                    
+                    
+			</div>
+             
+            </>
+        )
+    }
+} 
+
+
+export default ImgPark;
